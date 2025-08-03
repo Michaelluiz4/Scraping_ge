678e3d99c737bf34cd11e10b90ecede07ecaada3
Michael <michaelsb065@gmail.com>
2025-08-03 01:20:32 -0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import screapping
+
+def main():
+    screapping
+
+if __name__ == "__main__":
+    main()
---
+++ src/screapping.py
@@ -0,0 +1,38 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://ge.globo.com/futebol/times/palmeiras/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+subjects = soup.find_all("a", class_="feed-post-link")
+
+links = set()
+
+def reading_file():
+    if os.path.exists("links.json"):
+        try:
+            with open("links.json", "r", encoding="utf-8") as file:
+                data = json.load(file)
+                return set(data)
+        except json.JSONDecodeError:
+            pass
+    return set()
+
+def add_subjects(existing_links):
+    for subject in subjects:
+        link = subject["href"]
+        existing_links.add(link)
+    return existing_links
+
+
+def write_in_file(final_links):
+    with open("links.json", "w", encoding="utf-8") as file:
+        json.dump(list(final_links), file, ensure_ascii=False, indent=4)
+
+
+stored_links = reading_file()
+update_links = add_subjects(stored_links)
+write_in_file(update_links)
