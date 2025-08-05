d21dac82ada17da76cccd582e123874458e2d67d
Michael <michaelsb065@gmail.com>
2025-08-05 01:04:27 -0300

    scraping_ge

--- src/scraping.py
+++ src/scraping.py
@@ -3,38 +3,43 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://ge.globo.com/futebol/times/palmeiras/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+def run_scraping():
+    url = "https://ge.globo.com/futebol/times/palmeiras/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-subjects = soup.find_all("a", class_="feed-post-link")
-
-links = set()
-
-def reading_file():
-    if os.path.exists("links.json"):
-        try:
-            with open("links.json", "r", encoding="utf-8") as file:
-                data = json.load(file)
-                return set(data)
-        except json.JSONDecodeError:
-            pass
-    return set()
+    subjects = soup.find_all("a", class_="feed-post-link")
 
 
-def add_subjects(existing_links):
-    for subject in subjects:
-        href = subject.get("href")
-        if href:
-            existing_links.add(href)
-    return existing_links
+    def reading_file():
+        if os.path.exists("links.json"):
+            try:
+                with open("links.json", "r", encoding="utf-8") as file:
+                    data = json.load(file)
+                    return set(data)
+            except json.JSONDecodeError:
+                pass
+        return set()
 
 
-def write_in_file(final_links):
-    with open("links.json", "w", encoding="utf-8") as file:
-        json.dump(list(final_links), file, ensure_ascii=False, indent=4)
+    def extract_new_links(existing_links):
+        new_links = set()
+
+        for subject in subjects:
+            href = subject.get("href")
+            if href and href not in existing_links:
+                new_links.add(href)
+        return new_links
 
 
-stored_links = reading_file()
-update_links = add_subjects(stored_links)
-write_in_file(update_links)
+    def write_in_file(final_links):
+        with open("links.json", "w", encoding="utf-8") as file:
+            json.dump(list(final_links), file, ensure_ascii=False, indent=4)
+
+
+    stored_links = reading_file()
+    fresh_links = extract_new_links(stored_links)
+    all_links = stored_links.union(fresh_links)
+    write_in_file(all_links)
+
+    return list(fresh_links)
