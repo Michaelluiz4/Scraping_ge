664955118feb547eff972f5fdfae560f23deb471
Michael <michaelsb065@gmail.com>
2025-08-06 01:35:34 -0300

    main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from scraping import run_scraping
 from bot_telegram import send_message
 
 def main():
+    # main function
     new_links = run_scraping()
     for link in new_links:
         send_message(link)
